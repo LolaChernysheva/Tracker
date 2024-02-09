04628c6490089cab8b13cc72ee3ff3fe5755368f
LolaChernysheva <lolita-chernysheva@mail.ru>
2024-02-09 21:57:25 +0700

    Add assets, add launch screen

---
+++ Tracker/Tracker/Assets.swift
@@ -0,0 +1,20 @@
+//
+//  Assets.swift
+//  Tracker
+//
+//  Created by Lolita Chernysheva on 09.02.2024.
+//  
+//
+
+import UIKit
+
+struct Assets {
+    
+    struct Colors {
+        static let launchBlue = UIColor(named: "Launch")
+    }
+    
+    struct Images {
+        static let logo = UIImage(named: "Logo")
+    }
+}
